21733552c16931c26a8d3aeda20818e20f1e2e60
Landysh Khusaenova <landysh.kg@gmail.com>
2023-11-16 15:38:51 +0100

    update file pokritie.js

--- pokritie.test.js
+++ pokritie.test.js
@@ -1,29 +1,28 @@
 allStudents = require("./students.json");
 const maxScoreStudentFinding = require("./pokritie");
 
-
 describe("Тестирование работы функции", () => {
   it("Максимальное значение баллов для первого эл-та из тестовых данных", () => {
     const result = [
-        'Максимальное количество баллов: Ivan - 35',
-        'Максимальное количество баллов: Stepan - 35'
-      ];
+      "Максимальное количество баллов: Ivan - 35",
+      "Максимальное количество баллов: Stepan - 35",
+    ];
     const expectValue = maxScoreStudentFinding(allStudents[0]);
     expect(expectValue).toEqual(result);
   });
 
   it("Максимальное значение баллов для второго эл-та из тестовых данных", () => {
     const result = [
-        'Максимальное количество баллов: Natalia - 25',
-        'Максимальное количество баллов: Marina - 25',
-        'Максимальное количество баллов: Dmitry - 25'
-      ];
+      "Максимальное количество баллов: Natalia - 25",
+      "Максимальное количество баллов: Marina - 25",
+      "Максимальное количество баллов: Dmitry - 25",
+    ];
     const expectValue = maxScoreStudentFinding(allStudents[1]);
     expect(expectValue).toEqual(result);
   });
 
   it("Максимальное значение баллов для третьего эл-та из тестовых данных", () => {
-    const result = [ 'Количество баллов всех студентов группы = 0' ];
+    const result = ["Количество баллов всех студентов группы = 0"];
     const expectValue = maxScoreStudentFinding(allStudents[2]);
     expect(expectValue).toEqual(result);
   });
